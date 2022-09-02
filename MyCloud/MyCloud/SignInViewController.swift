2131b7285c3c938998e78c9bb6ef2cfd4bdd0747
LolaChernysheva <lolita-chernysheva@mail.ru>
2022-09-02 16:05:30 +0300

    Storyboard removed

---
+++ MyCloud/MyCloud/SignInViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignInViewController.swift
+//  MyCloud
+//
+//  Created by Лолита Чернышева on 02.09.2022.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
