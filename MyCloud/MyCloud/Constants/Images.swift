265299a0949308441260cc31437c8bac687d6682
LolaChernysheva <lolita-chernysheva@mail.ru>
2022-09-02 16:38:38 +0300

    Added assets

---
+++ MyCloud/MyCloud/Constants/Images.swift
@@ -0,0 +1,20 @@
+//
+//  Images.swift
+//  MyCloud
+//
+//  Created by Лолита Чернышева on 02.09.2022.
+//
+
+import UIKit
+
+final class Images {
+    static let filter = UIImage(named: "Filter")
+    static let file = UIImage(named: "File")
+    static let folder = UIImage(named: "Folder")
+    static let documents = UIImage(named: "Documents")
+    static let addFile = UIImage(named: "Add file")
+    static let addFolder = UIImage(named: "Add folder")
+    static let exit = UIImage(named: "Exit")
+    static let menu = UIImage(named: "Menu")
+    static let photo = UIImage(named: "Photo")
+}
